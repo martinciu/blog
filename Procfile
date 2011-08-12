a5849e9ee2906a6aef3e7a2950e5620a1d295165
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2011-08-12 21:44:11 +0200

    twitter stream

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 jekyll:  bundle exec jekyll --server
-comapss: bundle exec compass watch -e production -s compressed
+comapss: bundle exec compass watch 
--- javascripts/application.js
+++ javascripts/application.js
@@ -10,29 +10,40 @@ $(function() {
   $(".post .content a").attr("target","_blank");
 
   $.getJSON("http://www.coderwall.com/martinciu.json?callback=?", function(data) {
-
-  		$.each(data.data.badges, function(i, item) {
-
-  			$("<img/>").attr("src", item.badge)
-  			.attr("float", "left")
-  			.attr("title", item.name + ": " + item.description)
-  			.attr("alt", item.name)
-  			.attr("height", 75)
-  			.attr("width", 75)
-  			.appendTo("#coderwall")
-  			.hover(
-  				function(){  
-  					$(this).css("opacity","0.6");
-  				},
-  				function(){
-  					$(this).css("opacity","1.0");
-  				}
-  			)
-  			.click(
-  				function(){
-  					window.location = "http://www.coderwall.com/martinciu/";
-  				}
-  			);		
-  		});
-  	});
+    $.each(data.data.badges, function(i, item) {
+      $("#coderwall").append(
+        $("<a />")
+          .attr("href", "http://www.coderwall.com/martinciu/")
+          .attr("target", "_blank")
+          .append(
+            $("<img/>").attr("src", item.badge)
+              .attr("float", "left")
+              .attr("title", item.name + ": " + item.description)
+              .attr("alt", item.name)
+              .attr("height", 75)
+              .attr("width", 75)
+              .hover(
+                function(){  
+                  $(this).css("opacity","0.6");
+                },
+                function(){
+                  $(this).css("opacity","1.0");
+                }
+              )
+          )
+        );
+      });
+  });
+  $("#tweets").tweet({
+    username: "martinciu",
+    join_text: "auto",
+    avatar_size: 32,
+    count: 10,
+    auto_join_text_default: "we said,", 
+    auto_join_text_ed: "we",
+    auto_join_text_ing: "we were",
+    auto_join_text_reply: "we replied to",
+    auto_join_text_url: "we were checking out",
+    loading_text: "loading tweets..."
+  });
 });
